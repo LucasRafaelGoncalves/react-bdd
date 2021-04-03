526f28b58196135f8cdb251e73fb1e795608faa8
Lucas Rafael Gonçalves <lrafaelgoncalves@gmail.com>
2021-04-03 12:09:25 -0300

    feat: adds sum a pair feature

---
+++ src/features/step_definitions/sum_a_pair.steps.test.ts
@@ -0,0 +1,22 @@
+import { defineFeature, loadFeature } from 'jest-cucumber'
+import sum from '../../utils/sum'
+
+const feature = loadFeature('./src/features/sum_a_pair.feature')
+
+defineFeature(feature, test => {
+  test('adds 1 + 2 to equal 3', ({ given, when, then }) => {
+    let x: number, z: number
+
+    given('1', () => {
+      x = 1
+    })
+
+    when('add 2', () => {
+      z = sum(x, 2)
+    })
+
+    then('the sum is 3', () => {
+      expect(z).toBe(3)
+    })
+  })
+})
